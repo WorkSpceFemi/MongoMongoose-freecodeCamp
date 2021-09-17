5f7066fd6358b2da3a77e4a572d89136e6adea82
Femi Francis <CS5997@cloudium.local>
2021-09-17 10:17:56 +0530

    code for update and remove data

--- myApp.js
+++ myApp.js
@@ -86,12 +86,20 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name:personName},{age:ageToSet},{new:true},(err,updatedDoc)=>{
+    if(err) console.log(err);
+    done(null,updatedDoc);
+  })
+  //done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId,(err,removedData)=>{
+    if(err) console.log(err);
+    done(null,removedData);
+    
+  })
+  //done(null /*, data*/);
 };
 
 const removeManyPeople = (done) => {
