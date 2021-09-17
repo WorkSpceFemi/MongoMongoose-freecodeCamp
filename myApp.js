f95c3c236aff90c73c4fd7861cef9d59c2ff06ee
Femi Francis <CS5997@cloudium.local>
2021-09-17 09:25:32 +0530

    code for find a person by id

--- myApp.js
+++ myApp.js
@@ -59,7 +59,11 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({_id: personId}, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
+  //done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
